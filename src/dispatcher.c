b8410b3772c7d3726cf5e3e49790df965a231936
Mason Brainard <masonbrainard@isengard.mines.edu>
2020-10-08 21:13:16 -0600

    Commit at 9:13

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #include "dispatcher.h"
 #include "shell_builtins.h"
 #include "parser.h"
+
+int runcmd(struct command *pipeline, int prev_pipe[2], int stdout_cp[2]);
 
 /**
  * dispatch_external_command() - run a pipeline of commands
@@ -21,7 +25,7 @@
  *              observe the layout of this structure for a variety of
  *              inputs.
  *
- * Note: this function should not return until all commands in the
+ * Note: this function should not return un;til all commands in the
  * pipeline have completed their execution.
  *
  * Return: The return status of the last command executed in the
@@ -29,48 +33,114 @@
  */
 static int dispatch_external_command(struct command *pipeline)
 {
+	int std_cpy[2] = {dup(STDOUT_FILENO), dup(STDIN_FILENO)};
 	int status = 0;
-	pid_t pid = fork();
-	
-	if	(pid == -1) {
-		exit(EXIT_FAILURE); //bad things have happened
+
+	status = runcmd(pipeline, std_cpy, std_cpy);
+
+	//reassert stdout and stdin if they were changed
+	dup2(std_cpy[0], STDIN_FILENO);
+	dup2(std_cpy[1], STDOUT_FILENO);
+
+	return status;
+}
+
+int runcmd(struct command *pipeline, int prev_pipe[2], int std_cp[2])
+{
+	int status = 0;
+	int inputfile = 0;
+	int outputfile = 0;
+
+	//laying pipe
+	int pipeRW[2];
+	if (pipe(pipeRW) == -1) {
+		fprintf(stderr, "Unable to lay pipe.\n");
+		return -1;
 	}
-	else if (pid > 0) //parent 
-	{
-		waitpid(pid, &status, 0);
+
+	//if input_file change
+	if (pipeline->input_filename != NULL) {
+		if ((inputfile = open(pipeline->input_filename, O_RDONLY, S_IRUSR)) == -1) {
+			fprintf(stderr, "Error opening input file.\n");
+			return 1;
+		}
+		dup2(inputfile, STDIN_FILENO);
+	} else {
+		dup2(prev_pipe[0], STDIN_FILENO);
 	}
-	else //child
+
+	//check output_type
+	if (pipeline->output_type == COMMAND_OUTPUT_PIPE)
+		dup2(pipeRW[1], STDOUT_FILENO);
+	else if (pipeline->output_type == COMMAND_OUTPUT_FILE_APPEND) {
+		int append = O_WRONLY | O_APPEND;
+		if ((outputfile = open(pipeline->output_filename, append, S_IWUSR)) == -1) {
+			fprintf(stderr, "Error opening output file to append.\n");
+			return 1;
+		}
+		/*
+		if(dup2(outputfile, STDOUT_FILENO) == -1)
+		{
+			fprintf(stderr, "dup2 failed from Append.\n");
+			return 1;
+		}*/
+		dup2(outputfile, STDOUT_FILENO);
+	}
+	else if(pipeline->output_type == COMMAND_OUTPUT_FILE_TRUNCATE)
 	{
+		int truncate = O_WRONLY | O_TRUNC | O_CREAT;
+		if ((outputfile = open(pipeline->output_filename, truncate, S_IWUSR)) == -1) {
+			fprintf(stderr, "Error opening output file to create/truncate.\n");
+			return 1;
+		}
+		if(dup2(outputfile, STDOUT_FILENO) == -1)
+		{
+			fprintf(stderr, "dup2 failed from Truncate.\n");
+			return 1;
+		}
+	}
+	else {
+		dup2(std_cp[1], STDOUT_FILENO);
+	}
+
+	//fork
+	pid_t pid;
+	if ((pid = fork()) == -1) {
+		fprintf(stderr, "Something is 'forking' wrong...\n");
+		return 1;
+	} 
+	else if (pid == 0) //child
+	{
+		close(pipeRW[0]); //close read end of pipe
 		status = execvp(pipeline->argv[0], pipeline->argv);
 		fprintf(stderr, "%s: Command not found\n", pipeline->argv[0]);
+		return 1;
+	} else //parent
+	{
+		close(pipeRW[1]); //close write end of pipe
+		waitpid(pid, &status, 0);
+		//close input and output files
+		if (inputfile != 0) {
+			dup2(pipeRW[0], STDIN_FILENO);
+			if(close(inputfile) == -1){
+				fprintf(stderr, "Error closing input file.\n");
+				return 1;
+			}
+		}
+		if (outputfile != 0) {
+			dup2(pipeRW[1], STDOUT_FILENO);
+			if(close(outputfile) == -1){
+				fprintf(stderr, "Error closing output file.\n");
+				return 1;
+			}
+		}
 	}
+
+	if (pipeline->pipe_to != NULL) {
+		status = runcmd(pipeline->pipe_to, pipeRW, std_cp);
+	}
+	
 	return status;
-	
-	/*
-	 * Note: this is where you'll start implementing the project.
-	 *
-	 * It's the only function with a "TODO".  However, if you try
-	 * and squeeze your entire external command logic into a
-	 * single routine with no helper functions, you'll quickly
-	 * find your code becomes sloppy and unmaintainable.
-	 *
-	 * It's up to *you* to structure your software cleanly.  Write
-	 * plenty of helper functions, and even start making yourself
-	 * new files if you need.
-	 *
-	 * For D1: you only need to support running a single command
-	 * (not a chain of commands in a pipeline), with no input or
-	 * output files (output to stdout only).  In other words, you
-	 * may live with the assumption that the "input_file" field in
-	 * the pipeline struct you are given is NULL, and that
-	 * "output_type" will always be COMMAND_OUTPUT_STDOUT.
-	 *
-	 * For D2: you'll extend this function to support input and
-	 * output files, as well as pipeline functionality.
-	 *
-	 * Good luck!
-	 */
-	
 }
 
 /**
